9fd342e3467156b592f429b8d8f1420e607482e4
manu <707106175@qq.com>
2019-06-21 17:40:37 +0800

    原生Google版本SDK

---
+++ Assets/MainThreadQueue.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Author:李传礼
+ * DateTime:2017.12.26
+ * Description:主线程队列（主要用于解决Native回调结果并非在主线程执行会引起的问题）
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainThreadQueue : MonoBehaviour
+{
+    public static Queue<Action> ExecuteQueue = new Queue<Action>();
+    
+	void Start ()
+    {
+
+    }
+
+	void Update ()
+    {
+		while(ExecuteQueue.Count > 0)
+        {
+            Debug.Log("主线程队列执行");
+            ExecuteQueue.Dequeue().Invoke();
+        }
+	}
+}
